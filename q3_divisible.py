ef187420bbadca109841ed402f1db1c19717e445
Gaurav-pal23 <gauravpal1974@gmail.com>
2026-04-09 10:31:18 +0530

    Remove unused code and comments from q3_divisible.py

--- q3_divisible.py
+++ q3_divisible.py
@@ -13,28 +13,3 @@ else:
 Output:
 
 Yes
-
-
-
-
-✅ Question 5: Largest Number
-Problem:
-Find largest among three numbers.
-Code:
-Python
-a = 10
-b = 25
-c = 15
-
-# Find largest manually
-largest = a
-
-if b > largest:
-    largest = b
-if c > largest:
-    largest = c
-
-print(largest)
-Output:
-
-25
