d01058fa9916db23ea21dad47939c941a572c7ad
Gaurav-pal23 <gauravpal1974@gmail.com>
2026-04-09 10:19:15 +0530

    Add bonus calculation for salary
    Implement bonus calculation based on salary.

---
+++ q1_bonus.py
@@ -0,0 +1,14 @@
+Question 1: Bonus Calculation
+Problem:
+Calculate bonus based on salary.
+Code:
+Python
+salary = 45000
+
+# Bonus is 7% of salary
+bonus = salary * 0.07
+
+print("Bonus =", int(bonus))
+Output:
+
+Bonus = 3150
