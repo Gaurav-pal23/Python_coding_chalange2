76f6e5b1375f58113a4e7a378137c1d0e03d1f98
Gaurav-pal23 <gauravpal1974@gmail.com>
2026-04-09 10:20:32 +0530

    Add age classification for Minor, Adult, and Senior
    Implement age classification logic to categorize users as Minor, Adult, or Senior based on their age.

---
+++ q2_age.py
@@ -0,0 +1,17 @@
+Question 2: Age Classification
+Problem:
+Classify user as Minor, Adult, or Senior.
+Code:
+Python
+age = 65
+
+# Check age category
+if age < 18:
+    print("Minor")
+elif age < 60:
+    print("Adult")
+else:
+    print("Senior")
+Output:
+
+Senior
