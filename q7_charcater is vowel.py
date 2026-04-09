23716687875c70f56a9f09092c9fd8afe397a23b
Gaurav-pal23 <gauravpal1974@gmail.com>
2026-04-09 10:36:53 +0530

    Add vowel checking functionality
    This script checks if a given character is a vowel or consonant.

---
+++ q7_charcater is vowel.py
@@ -0,0 +1,17 @@
+###------- Problem:
+Check if character is vowel.
+Code:
+Python
+char = 'e'
+
+# Convert to lowercase
+char = char.lower()
+
+# Check vowel
+if char in ['a', 'e', 'i', 'o', 'u']:
+    print("Vowel")
+else:
+    print("Consonant")
+Output:
+
+Vowel
