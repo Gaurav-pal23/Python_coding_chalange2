304d10912c7414da6435085f275d45c304d16abc
Gaurav-pal23 <gauravpal1974@gmail.com>
2026-04-09 10:42:18 +0530

    Add number type checker in q9_number_type.py
    Implement number type checker for positive, negative, or zero.

---
+++ q9_number_type.py
@@ -0,0 +1,16 @@
+###------- Problem:
+Check if number is positive, negative, or zero.
+Code:
+Python
+num = -5
+
+# Check number type
+if num > 0:
+    print("Positive")
+elif num < 0:
+    print("Negative")
+else:
+    print("Zero")
+Output:
+
+Negative
