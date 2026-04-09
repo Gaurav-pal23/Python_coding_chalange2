6f2632b3d8c3538b5a01736c7e2c6449579500c5
Gaurav-pal23 <gauravpal1974@gmail.com>
2026-04-09 10:44:23 +0530

    Add ticket price determination logic
    Implement ticket pricing based on the day of the week.

---
+++ q10_ticket.py
@@ -0,0 +1,16 @@
+###--------problem:
+Determine ticket price based on day.
+Code:
+Python
+day = "Sunday"
+
+# Set ticket price
+if day == "Sunday":
+    price = 200
+else:
+    price = 150
+
+print(price)
+Output:
+
+200
