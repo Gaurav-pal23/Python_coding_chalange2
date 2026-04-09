e67029a836410a8c0a4d700fa234b3712e14adfa
Gaurav-pal23 <gauravpal1974@gmail.com>
2026-04-09 10:50:49 +0530

    Add initial code for counting numbers greater than 50

---
+++ q13_ Count Numbers > 50.py
@@ -0,0 +1,16 @@
+####----problem
+Code:
+Python
+nums = [10, 60, 30, 80]
+
+count = 0
+
+# Count numbers greater than 50
+for n in nums:
+    if n > 50:
+        count += 1
+
+print(count)
+Output:
+
+2
